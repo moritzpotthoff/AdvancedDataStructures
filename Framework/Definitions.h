50ebed3e8cfcf6760d905cb28b091c48405c777f
mptf <moritz.potthoff@gmx.de>
2022-07-24 23:23:33 +0200

    Work on presentation, update configuration.

--- Framework/Definitions.h
+++ Framework/Definitions.h
@@ -1,4 +1,4 @@
 //w must be chosen such that w * w / 2 and w * w * 2 are multiples of 64, i.e., w must be a multiple of 16
-#define wBV 96
-#define wBP 64
+#define wBV 224
+#define wBP 192
 #define inf 2147483647
